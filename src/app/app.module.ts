0d250367e95c5831c3d279dac8cc9b6853d47711
gunturpp <guntur_34@apps.ipb.ac.id>
2018-07-05 20:41:03 +0700

    add innerHTML in article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,9 +27,6 @@ import { LoadingProvider } from '../providers/loading';
 import { FileTransfer } from '@ionic-native/file-transfer';
 
 import { SignupPage } from '../pages/signup/signup';
-import { AlertProvider } from '../providers/alert/alert';
-import { ToastProvider } from '../providers/toast/toast';
-
 //set the auth http for API
 export function getAuthHttp(http, Storage) {
   return new AuthHttp(new AuthConfig({
@@ -92,8 +89,6 @@ export function getAuthHttp(http, Storage) {
       useFactory: getAuthHttp,
       deps: [Http, Storage]
     },
-    AlertProvider,
-    ToastProvider
   ]
 })
 export class AppModule {}
